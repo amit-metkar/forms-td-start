991a31d90f7e364ff404f17b4b12ad2a017bd739
Amit Metkar <amit.metkat@gmail.com>
2017-09-04 22:29:44 +0530

    submit form and extract data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,28 @@ export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
   defaultQuestion = 'pet';
   answer = '';
+  genders = ['Male', 'Female'];
+
+  user = {
+    userName: '',
+    email: '',
+    secret: '',
+    answer: '',
+    gender: ''
+  };
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: 'amit.metkar@gmail.com'
+    //   },
+    //   secret: 'pet',
+    //   gender: 'Male',
+    //   questionAnswer: ''
+    // });
+    this.signupForm.form.patchValue({'userData': {'username': suggestedName}});
   }
 
   // onSubmit(form: NgForm) {
@@ -22,5 +41,10 @@ export class AppComponent {
 
   onSubmit() {
     console.log(this.signupForm);
+    this.user.userName = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secret = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
   }
 }
